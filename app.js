c091d7ad1763e3e332a5f6444d597cdb6ba8ad41
Martin-Oc <ockaymartin@gmail.com>
2022-06-21 21:10:17 +0200

    hotovy modal

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-const navbar = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
-// add fixed class to navbar
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 80) {
-    navbar.classList.add("navbar-fixed");
-  } else {
-    navbar.classList.remove("navbar-fixed");
-  }
-});
+
 // show sidebar
 navBtn.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
@@ -19,4 +11,30 @@ closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
 // set year
-date.innerHTML = new Date().getFullYear();
+const datum = new Date().getFullYear();
+date.innerHTML = datum
+
+
+//modal
+
+const obrazokGaleria = document.querySelectorAll("#img-galeria")
+const modal = document.querySelector(".modal")
+const modalObrazok = document.querySelector("#image-modal")
+const closeModalBtn = document.querySelector("#close-btn-modal")
+
+
+obrazokGaleria.forEach(function(image){ 
+  image.addEventListener("click",OpenModal)
+})
+
+closeModalBtn.addEventListener("click",function(){
+  modal.classList.remove("open")
+})
+
+function OpenModal(e){
+  const currentImageSrc = e.currentTarget.src
+  modal.classList.add("open")
+  modalObrazok.src = currentImageSrc
+}
+
+
