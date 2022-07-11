8a590f12d2ac78e0c6bfef756386cba667414fe3
Martin-Oc <ockaymartin@gmail.com>
2022-07-11 22:40:38 +0200

    modal nadhlad obrazov v modale

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import "./modal.js"
+
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
@@ -14,50 +16,6 @@ closeBtn.addEventListener("click", function () {
 const datum = new Date().getFullYear();
 date.innerHTML = datum
 
-
-//modal
-
-const obrazokGaleria = document.querySelectorAll("#img-galeria")
-const modal = document.querySelector(".modal")
-const modalObrazok = document.querySelector("#image-modal")
-const closeModalBtn = document.querySelector("#close-btn-modal")
-const modalText = document.querySelector("#text-modal")
-const modalTextDiv = document.querySelector(".modal-text")
-
-obrazokGaleria.forEach(function(image){ 
-  image.addEventListener("click",OpenModal)
-})
-
-if(modal){
-closeModalBtn.addEventListener("click",function(){
-  modal.classList.remove("open")
-})
-}
-
-function OpenModal(e){
-  const currentImageSrc = e.currentTarget.src
-  modal.classList.add("open")
-  modalObrazok.src = currentImageSrc
-
-  //meno obrazku 
-  const currentImageName = e.currentTarget.nextElementSibling.innerHTML
-  modalText.innerHTML = currentImageName
-
-  //pozicia nazvu obrazku
-  const topImage = modalObrazok.getBoundingClientRect().top
-  const textposition = topImage - 50
-  modalTextDiv.style.top = `${textposition}px`;
-  
-  //close modal iwh tab on background
-  modal.addEventListener("click",function(e){
-    const clickonbackground = e.target
-    if(clickonbackground == modal){
-    modal.classList.remove("open")
-    }
-  })
-
-}
-//end modal
 
 
 
